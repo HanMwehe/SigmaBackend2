f6b91e782a961df213b8127bb6c44c30955fe91b
LoveTs <han678556@gmail.com>
2025-06-22 13:12:27 +0700

    Update index.js

--- index.js
+++ index.js
@@ -43,5 +43,5 @@ app.use('/tugas-submission', tugasSubmissionRoutes);
 app.use('/ujian-submission', ujianSubmissionRoutes);
 app.use('/feedback', feedbackRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+export default app;
+
